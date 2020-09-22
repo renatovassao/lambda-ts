9f9122ba569db5f008a67d4371363f7732796384
Renato Vassão <renatomvd@hotmail.com>
2020-09-22 00:37:42 -0300

    fix: correct Monad return type parameter

--- src/types/monad.ts
+++ src/types/monad.ts
@@ -1,6 +1,6 @@
 import { Functor } from "./functor";
 
 export interface Monad<T> extends Functor<T> {
-  return(t: T): Monad<T>;
+  return<U>(u: U): Monad<U>;
   bind<U>(f: (t: T) => Monad<U>): Monad<U>;
 }
